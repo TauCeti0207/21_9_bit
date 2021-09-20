f9be261d3df7b3140f9bcec6329022ad25d2a67b
TauCeti0207 <81548397+TauCeti0207@users.noreply.github.com>
2021-09-20 22:45:51 +0800

    Add files via upload

---
+++ game.h
@@ -0,0 +1,19 @@
+#define ROW 3
+#define COL 3
+#include<stdio.h>
+#include<stdlib.h>	
+//srand要用到<stdlib.h>	
+#include<time.h>
+//函数声明
+void InitBoard(char board[ROW][COL], int row, int col);
+void DisplayBoard(char board[ROW][COL],int row,int col);
+void PlayerMove(char board[ROW][COL], int row, int col);
+void ComputerMove(char board[ROW][COL], int row, int col);
+
+
+//返回四种游戏状态
+//玩家赢-'*'
+//电脑赢-'#'
+//平局	-'Q
+//继续	-'C'
+char IsWin(char board[ROW][COL], int row, int col);
