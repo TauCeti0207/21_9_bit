99fec716d9cbce950a72b3135361eb8494df2673
TauCeti0207 <2076188013@qq.com>
2022-01-23 00:19:20 +0800

    22-1-23-内存函数“ git commit -m 22-1-23-内存函数“

---
+++ test_22_1_22/test.c
@@ -0,0 +1,97 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//int main()
+//{
+//	char ch = '0';
+//	if (ch >= '0' && ch <= '9')
+//	{
+//		//...
+//	}
+//	if (isdigit(ch))
+//	{
+//		//...这样代码才更加统一
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	char ch = 0;
+//	ch = getchar();
+//	if (islower(ch))
+//	{
+//		ch = toupper(ch);
+//	}
+//	else
+//	{
+//		ch = tolower(ch);
+//	}
+//	printf("%c\n", ch);
+//	return 0;
+//}
+// 
+#include<stdio.h>
+#include<ctype.h>
+#include<assert.h>
+void* my_memcpy(void* dest, const void* src, size_t num)
+{
+    void* ret = dest;
+    assert(dest && src);
+    while (num--)
+    {
+        *(char*)dest = *(char*)src;//强制类型转换只是临时的，并不改变dest的类型
+        dest = (char*)dest + 1;;//void*不能直接++
+        src = (char*)src + 1;
+    }
+    return ret;
+}
+
+#include<stdio.h>
+#include<memory.h>
+#include<string.h>
+#include<assert.h>
+
+
+void* my_memmove(void* dest, const void* src, size_t num)
+{
+    void* ret = dest;
+    assert(dest && src);
+
+    //从前向后拷贝
+    if (dest < src)
+    {
+        while (num--)
+        {
+            *(char*)dest = *(char*)src;
+            dest = (char*)dest + 1;
+            src = (char*)src + 1;
+        }
+    }
+
+    //从后向前拷贝
+    else
+    {
+        while (num--)//20就变成19
+        {
+            *((char*)dest + num) = *((char*)src + num);
+        }
+    }
+    return ret;
+}
+void test1()
+{
+    //将arr3中的12345拷贝放到34567中
+    int arr3[] = { 1,2,3,4,5,6,7,8,9,10 };
+    int arr4[5] = { 0 };
+    my_memmove(arr3+2, arr3, 5 * sizeof(arr3[0]));
+    for (size_t i = 0; i < 10; i++)
+    {
+        printf("%d ", arr3[i]);//1 2 1 2 3 4 5 8 9 10
+    }
+}
+int main()
+{
+   
+    test1();
+    return 0;
+}
